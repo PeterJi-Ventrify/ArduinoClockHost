564ea8ab52684197025a6cec06966fe7305a4777
PeterJi-Ventrify <peter.ji@ventrify.ca>
2021-10-29 22:53:19 -0600

    Add files via upload

---
+++ Host Machine (C#)/Winform Demo/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace Winform_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(254, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(131, 74);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(117, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(47, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Baudrate";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(47, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Com Port";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(-1, 107);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(416, 39);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "dd/mm/yyyy hh:mm:ss A/P";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(143, 31);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(75, 24);
+            this.comboBox2.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(254, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 24);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Check Ports";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 161);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.Name = "Form1";
+            this.Text = "Timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Host Machine (C#)/Winform Demo/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.IO.Ports;
+
+namespace Winform_Demo
+{
+    public partial class Form1 : Form
+    {
+        int cnt = 0;
+        int time = 0;
+        int[] baudrates = { 1200, 2400, 4800, 9600, 19200, 57600, 115200 };
+        string[] ports;
+        DateTime dateTime;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Upon loading the window, search for serial ports
+            // And add them to the drop down list
+            ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                comboBox2.Items.Add(port);
+            }
+            // Fill comboBox1 with common baudrates
+            foreach (int baudrate in baudrates)
+            {
+                comboBox1.Items.Add(baudrate);
+            }
+            // Set the default baudrate
+            comboBox1.SelectedItem = 115200;
+            // Set the defualt COM port to the last added
+            comboBox2.SelectedIndex = ports.Length - 1;
+            // Initilaze 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            cnt++;
+            if (cnt == time)
+            {
+                timer1.Stop();
+                button1.Text = "Start";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Connect")
+            {
+                // When Connect is clicked, set serial port parameters
+                // and establish connection
+                serialPort1.PortName = comboBox2.Text;
+                serialPort1.BaudRate = (int)comboBox1.SelectedItem;
+                serialPort1.Open();
+                button1.Text = "Disconnect";
+                timer2.Start();
+            }
+            else
+            {
+                serialPort1.Close();
+                button1.Text = "Connect";
+                timer2.Stop();
+            }
+            
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            // Update label6 with current date and time\
+            dateTime = DateTime.Now;
+            label6.Text = Convert.ToString(dateTime) + Convert.ToString(dateTime.DayOfWeek);
+            // Send the string over serialPort1
+            if (serialPort1.IsOpen) serialPort1.Write(label6.Text);
+            else button1_Click(sender, e);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ports = SerialPort.GetPortNames();
+            comboBox2.Items.Clear();
+            foreach (string port in ports)
+            {
+                comboBox2.Items.Add(port);
+            }
+            comboBox2.SelectedIndex = ports.Length - 1;
+        }
+    }
+}
